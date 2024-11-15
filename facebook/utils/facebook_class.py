723721e5e85a8eef62647afb8f899f51a78ae8ed
AxierPer <axierperlaz2018@gmail.com>
2024-11-15 10:22:04 -0500

    In this file, replace de class funtions, and change insert for a bulk data in db, and solvent the problem to pool with postgres an SQLALCHEMY

--- facebook/utils/facebook_class.py
+++ facebook/utils/facebook_class.py
@@ -1,25 +1,29 @@
 from datetime import date
-from sqlalchemy import insert, select
+from sqlalchemy import select
 import  json
+from  typing import List
 from facebook.database.create_session import get_db
-from facebook.utils.database_models import Facebook_adset_data
+from facebook.utils.database_models import Facebook_adset_data, Facebook_ads_data
 
 class Adsets_facebook:
-    def insert_data (self, page, id_page, name_adset, daily_budget, id_adset, date_start, date_stop):
-        self.page = page
-        self.id_page = id_page
-        self.name_adset = name_adset
-        self.daily_budget = daily_budget
-        self.id_adset = id_adset
-        self.date_start = date_start
-        self.date_stop = date_stop
-        
+    def insert_data (self, data_list: List[dict]):
+        adsets = [
+            Facebook_adset_data(
+                page= item["page"],
+                name=item["name"],
+                id_page=item["id_page"],
+                daily_budget=item["daily_budget"],
+                adset_id=item["id"],
+                date_start=item["date_start"],
+                date_stop=item["date_stop"]
+            )
+            for item in data_list
+        ]
+
         db = next(get_db()) # Create Session
-        new_adset = Facebook_adset_data(page= self.page, name=self.name_adset, id_page=self.id_page, daily_budget=self.daily_budget, adset_id=self.id_adset, date_start=self.date_start, date_stop=self.date_stop)
-        db.add(new_adset) # Add data to stage
+        db.bulk_save_objects(adsets)
         db.commit() # Transaction comfirm
-        db.refresh(new_adset) # Refresh state of new_adset
-        return new_adset
+        return adsets
 
     def get_data(self, date_stop_query):
         db = next(get_db())
@@ -37,7 +41,56 @@ class Adsets_facebook:
                     result_dict[key] = value.isoformat()  # Convierte la fecha a formato string ISO (YYYY-MM-DD)
 
             data_list.append(result_dict)
-        
+
         adset_data =  json.dumps(data_list, indent=4)
         return adset_data
+
+
+class Ad_Id_facebook:
+    def insert_data(self, data_list: List[dict]):
+        ad_ids = [
+            Facebook_ads_data(
+                page=item["page"],
+                ad_name=item["ad_name"],
+                adset_budget=item["adset_budget"],
+                results=item["results"],
+                cost_per_result=item["cost_per_result"],
+                reach=item["reach"],
+                impressions=item["impressions"],
+                amount_spend=item["spend"],
+                cost_per_1000_accounts_centre_accounts_reached=item["cpm"],
+                ctr_all=item["ctr"],
+                frequency=item["frequency"],
+                ad_id=item["ad_id"],
+                date_start=item["date_start"],
+                date_stop=item["date_stop"]
+            )
+            for item in data_list
+        ]
+        db = next(get_db())
+        db.bulk_save_objects(ad_ids)
+        db.commit()
+        return ad_ids
+
+
+    def get_data(self, date_stop_query):
+        db = next(get_db())
+        query = select(Facebook_ads_data).where(Facebook_ads_data.date_stop == date_stop_query).order_by(Facebook_ads_data.page)
+        results = db.execute(query).scalars().all()
+
+    # Convertir los resultados a JSON-friendly data
+        data_list = []
+        for result in results:
+            result_dict = {column.name: getattr(result, column.name) for column in result.__table__.columns}
+
+            # Convertir fechas a string
+            for key, value in result_dict.items():
+                if isinstance(value, date):
+                    result_dict[key] = value.isoformat()  # Convierte la fecha a formato string ISO (YYYY-MM-DD)
+
+            data_list.append(result_dict)
+        
+        ads_data =  json.dumps(data_list, indent=4)
+        return ads_data
+
 
